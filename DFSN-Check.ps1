cd47663377c0553cdef99929720d3ea413dde5b9
Anupam Majhi <anupammajhi@users.noreply.github.com>
2017-11-15 23:29:29 +0530

    Add Comments; Correct Flush

--- DFSN-Check.ps1
+++ DFSN-Check.ps1
@@ -5,14 +5,29 @@ $root = "myRoot"
 
 $NameSpaceServers = dfsutil client property state \\$domain\$root | %{$_.trimstart("Active, ").trimstart("Online ")} | ?{$_ -like "*$root"}
 
-$DefaultNS = $NameSpaceServers[0]
 
 $NameSpaceServers | %{
+    
+    Write-host "`n`n`n`n====================================================" -ForegroundColor DarkGreen
+
     Write-Host "Checking $_" -ForegroundColor Green
     dfsutil client property state active \\$domain\$root $_
-    dfsutil client property state \\$domain\$root
+
+    Write-host "Checking Active Server" -ForegroundColor Yellow
+    $ActiveServer = (dfsutil client property state \\$domain\$root | %{$_.trimstart("Active, ").trimstart("Online ")} | ?{$_ -like "*$root"})[0]
+    Write-Host "Active-Server : $ActiveServer"
+
+    $targetCount = ls \\$domain\$root | Measure-Object | select -ExpandProperty count
+    Write-Host $targetCount
+    if($targetCount -gt 0){
+        Write-Host "Target Accessible" -ForegroundColor Green
+    }
+    else{
+        Write-Host "Target NOT Accessible"  -ForegroundColor Red
+    }
 }
 
 #Resetting Active Referral Namespace Server to as it was before test
-dfsutil client property state active \\$domain\$root $DefaultNS
+    dfsutil cache referral flush
     dfsutil client property state \\$domain\$root
+    
